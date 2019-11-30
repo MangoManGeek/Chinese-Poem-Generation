9457797b82568cd1ab798c8d62417d4699a33f0c
Yue Sun <ysun276@wisc.edu>
2019-11-30 00:02:45 -0500

    runnable model and checkpoint implementation

--- main.py
+++ main.py
@@ -3,23 +3,10 @@
 
 from generateModel import GenerateModel
 from plan import Planner
-from paths import save_dir
-import tensorflow as tf
-import sys
 
 if __name__ == '__main__':
     planner = Planner()
     generator = GenerateModel()
-    if not tf.train.get_checkpoint_state(save_dir):
-        print("Please train the model first! (./train.py -g)")
-        sys.exit(1)
-
-    checkpoint = tf.train.Checkpoint(generator=generator)
-    try:
-        checkpoint.restore(tf.train.latest_checkpoint(save_dir)).assert_consumed()
-        print("Checkpoint is loaded successfully !")
-    except AssertionError:
-        print("Fail to load checkpoint")
 
     while True:
         hints = input("Provide a title >> ")
