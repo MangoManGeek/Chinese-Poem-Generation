78f62f7ba6f852f2612daa4fa27c8517981e01b4
Yue Sun <yue_sun@brown.edu>
2019-12-09 16:04:33 -0500

    Update evaluate_poems.py

--- evaluate_poems.py
+++ evaluate_poems.py
@@ -17,45 +17,24 @@ for pair in ranked_words.word_scores:
     word_scores[pair[0]] = pair[1]
 
 
-def eval_poems(poems):
-    poem_scores = get_poem_scores()
-
+def eval_poems(poem, sorted_poem_scores1, sorted_poem_scores2, sorted_poem_scores3):
     rv_score = 0.0
 
     # process first lien
-
-    sorted_poem_scores = sorted(poem_scores, key=lambda curr_poem: curr_poem.score1)
-
-    for poem in poems:
-        target_score = get_score(poem[0])
-
-        target_poem = binarySearch(sorted_poem_scores, target_score, 1)
-
-        rv_score += abs(target_poem.score2 - target_score)
+    target_score = get_score(poem[0])
+    target_poem = binarySearch(sorted_poem_scores, target_score, 1)
+    rv_score += abs(target_poem.score2 - target_score)
 
     # process 2 lien
+    target_score = get_score(poem[1])
+    target_poem = binarySearch(sorted_poem_scores, target_score, 2)
+    rv_score += abs(target_poem.score3 - target_score)
 
-    sorted_poem_scores = sorted(poem_scores, key=lambda curr_poem: curr_poem.score2)
+    target_score = get_score(poem[2])
+    target_poem = binarySearch(sorted_poem_scores, target_score, 3)
+    rv_score += abs(target_poem.score4 - target_score)
 
-    for poem in poems:
-        target_score = get_score(poem[1])
-
-        target_poem = binarySearch(sorted_poem_scores, target_score, 2)
-
-        rv_score += abs(target_poem.score3 - target_score)
-
-    # process 3 lien
-
-    sorted_poem_scores = sorted(poem_scores, key=lambda curr_poem: curr_poem.score3)
-
-    for poem in poems:
-        target_score = get_score(poem[2])
-
-        target_poem = binarySearch(sorted_poem_scores, target_score, 3)
-
-        rv_score += abs(target_poem.score4 - target_score)
-
-    return rv_score / len(poems)
+    return rv_score
 
 
 def binarySearch(sorted_poem_scores, target_score, num_of_sentence):
@@ -120,9 +99,7 @@ class PoemScore:
 
 def get_poem_scores():
     poems = Poems()
-
     poem_scores = []
-
     # count=0
     for p in poems:
         # count+=1
@@ -143,65 +120,56 @@ def get_poem_scores():
 
 
 def get_score(sentence):
-    # ranked_words=RankedWords()
-
-    # word_scores=dict()
-    # for pair in ranked_words.word_scores:
-
-    #	word_scores[pair[0]]=pair[1]
-
     planner = Planner()
-
     highest_ranked_word = ''
     highest_ranked_score = 0.0
 
     for word in jieba.lcut(sentence):
-
         if word not in word_scores:
             continue
-
         if word_scores[word] > highest_ranked_score:
             highest_ranked_word = word
 
-    print("keyword is {w}".format(w=highest_ranked_word))
-
+    # print("keyword is {w}".format(w=highest_ranked_word))
     keyword = set()
     keyword.add(highest_ranked_word)
-
     score = 0.0
     count = 0
 
     for word in planner._expand(keyword):
-
         if word in word_scores:
-
             score += word_scores[word]
             count += 1
-
         else:
-
-            print('{w} does not have a score'.format(w=word))
+            # print('{w} does not have a score'.format(w=word))
 
     return score / count
 
 
 def main():
     planner = Planner()
-    generator = GenerateModel(True)
+    generator = GenerateModel(False)
 
-    # words = csv.reader(open(data_sxhy_path, 'r'), delimiter="\t")
+    poem_scores = get_poem_scores()
+    sorted_poem_scores1 = sorted(poem_scores, key=lambda curr_poem: curr_poem.score1)
+    sorted_poem_scores2 = sorted(poem_scores, key=lambda curr_poem: curr_poem.score2)
+    sorted_poem_scores3 = sorted(poem_scores, key=lambda curr_poem: curr_poem.score3)
+
+    avg_score = 0
+    num = 500
+
     for line in open(data_sxhy_path):
         listWords = line.split()
-    poems = []
-    for i in range(500):
+    for i in range(num):
         keyword = listWords[i]
         keywords = planner.plan(keyword)
-        poems += generator.generate(keywords)
-
-    score = eval_poems(poems)
-
-    print(score)
-
+        poem = generator.generate(keywords)
+        score = eval_poems(poem)
+        avg_score += score
+        for sentence in poem:
+            print(sentence)
+        print("The score of the current poem is:" + score)
+    print("The average score is:" + avg_score / num)
 
 if __name__ == '__main__':
     main()
