1da24461e8631311189903d7a839abdb9aefd4a9
MangoManGeek <32067306+MangoManGeek@users.noreply.github.com>
2019-12-14 17:26:30 -0500

    remove reference

--- generate.py
+++ generate.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-#########################################################################################################################################
-####### This source code is based on [DevinZ1993](https://github.com/DevinZ1993/Chinese-Poetry-Generation)'s implementation.      #######
-#########################################################################################################################################
-
 
 from char2vec import Char2Vec
 from char_dict import CharDict, end_of_sentence, start_of_sentence
